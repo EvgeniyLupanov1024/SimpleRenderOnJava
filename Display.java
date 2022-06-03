c790f92cd932a95f2610a05c3aa656e301d25470
Евгений Лупанов <e.lupanov@qsoft.ru>
2022-06-03 15:21:29 +0300

    render background

--- Display.java
+++ Display.java
@@ -1,6 +1,10 @@
 import java.awt.Canvas;
 import java.awt.Dimension;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
 import javax.swing.JFrame;
 
 public class Display extends Canvas implements Runnable
@@ -11,6 +15,7 @@ public class Display extends Canvas implements Runnable
     private static String title = "Рендрю кудик в 3d";
     private static final int WIDTH = 800;
     private static final int HEIGTH = 600;
+    private static Color bgColor = new Color(11, 22, 55);
 
     private static boolean running = false;
 
@@ -40,18 +45,75 @@ public class Display extends Canvas implements Runnable
     public void start()
     {
         running = true;
-
+        this.thread = new Thread(this, "Display");
+        this.thread.start();
     }
 
     public void stop()
     {
         running = false;
 
+        try {
+            this.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void run() {
-        // TODO Auto-generated method stub
+    public void run() 
+    {
+        long lastTimeUpdate = System.nanoTime();
+        final double nsUpdate = 1000000000.0 / 60;
+        
+        long lastTimeMs = System.currentTimeMillis();
+        int framesCount = 0;
+
+        init();
+
+        while (running)
+        {
+            if (System.nanoTime() - lastTimeUpdate > nsUpdate) {
+
+                lastTimeUpdate = System.nanoTime();
+
+                update();
+                render();
+                ++framesCount;
+            }
+
+            if (System.currentTimeMillis() - lastTimeMs > 1000) {
+                
+                lastTimeMs = System.currentTimeMillis();
+                this.frame.setTitle(title + " | " + framesCount + " fps");
+                framesCount = 0;
+            }
+        }
+    }
+
+    private void init()
+    {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(2);
+            return;
+        }
+    }
+
+    private void render()
+    {
+        BufferStrategy bs = getBufferStrategy();
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(bgColor);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.dispose();
+        bs.show();
+    }
+
+    private void update()
+    {
         
     }
 }
